f86d79a2d995d138504387ffa7a8f9e905ba3cca
PeeeeeteW <peter.q.c.wang@gmail.com>
2024-12-04 11:33:30 +0000

    First cutscene automatically trigger

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -7,38 +7,23 @@ public class StateManager : MonoBehaviour
 {
     public DialogueSet temp;
     public DialogueController dialogueController;
+    public bool cutsceneInProgress = false;
     public bool playerInControl = false;
     public bool puzzleOneSolved = false;
 
     private bool tempFlag = false;
 
 
-    public void EnablePlayerControl(bool value)
+    public void SetPlayerControl(bool value)
     {
         playerInControl = value;
     }
 
-
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
-        if(!tempFlag && Input.GetKeyDown(KeyCode.F))
-        {
-            temp.Trigger("1-1");
-            tempFlag = true;
-        }
-        else
-        {
-            if(tempFlag && Input.GetKeyDown(KeyCode.F))
+            if(cutsceneInProgress && Input.GetKeyDown(KeyCode.F))
             {
                 dialogueController.NextSentence();
             }
-
-        }
-        
     }
 }
