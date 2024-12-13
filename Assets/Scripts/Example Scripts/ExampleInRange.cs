c1d07133ae143a7253edb82578e7c4b07654d329
PeeeeeteW <peter.q.c.wang@gmail.com>
2024-12-13 18:35:10 +0000

    Added Example Scene for Maintenance Manual

---
+++ Assets/Scripts/Example Scripts/ExampleInRange.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ExampleInRange : MonoBehaviour
+{
+    private bool playerInRange = false;
+    public ExampleManager stateManager;
+    public DialogueController dialogueController;
+
+    void Start()
+    {
+        this.transform.parent.Find("PromptToPlayer").gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(playerInRange && Input.GetKeyDown(KeyCode.F) && !stateManager.playerInDialogue && !stateManager.playerInMCQ)
+        {
+            GetComponentInParent<DialogueSet>().Trigger(stateManager.phase);
+            stateManager.TakePlayerControl();
+        }
+
+        if(playerInRange && Input.GetKeyUp(KeyCode.F) && !stateManager.playerInDialogue && !stateManager.playerInMCQ)
+        {
+            stateManager.StartDialogue();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            this.transform.parent.Find("PromptToPlayer").gameObject.SetActive(true);
+            playerInRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            this.transform.parent.Find("PromptToPlayer").gameObject.SetActive(false);
+            playerInRange = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Example Scripts/ExampleMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ExampleMovement : MonoBehaviour
+{
+
+    public ExampleManager stateManager;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float runSpeed = 3f;
+    private Animator animator;
+
+    // class scoped variable
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(stateManager.playerInControl)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.AddForce(movement * speed * runSpeed);
+            }
+            
+            else
+            {
+                rb.AddForce(movement * speed);
+            }
+        }
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        // sve the corresponding vector 2
+        movement = value.Get<Vector2>();
+
+        // animator setfloat
+        if (movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
